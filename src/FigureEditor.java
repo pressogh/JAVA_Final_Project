9617c3a0e9fad9717cb47a3c32d8f792dfcebe10
PRESSO <caff1nepill@gmail.com>
2021-11-26 15:21:15 +0900

    Add Shape Classes & Draw Function

--- src/FigureEditor.java
+++ src/FigureEditor.java
@@ -2,9 +2,13 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Container;
 import java.awt.FlowLayout;
+import java.awt.Graphics;
 import java.awt.GridLayout;
+import java.awt.Point;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.util.Vector;
 
 import javax.swing.JButton;
@@ -29,9 +33,71 @@ public class FigureEditor extends JFrame {
 	}
 	
 	private class CenterPanel extends JPanel {
+		Point start, end;
+		Vector<Shape> shapeArray = new Vector<>();
 		public CenterPanel() {
 			setLayout(new FlowLayout());
 			setBackground(Color.YELLOW);
+
+			addMouseListener(new mouseListener());
+			addMouseMotionListener(new mouseListener());
+		}
+
+		public void paintComponent(Graphics g) {
+			super.paintComponent(g);
+			if(start == null)
+				return;
+			g.setColor(Color.BLUE);
+			int x = Math.min(start.x, end.x);
+			int y = Math.min(start.y, end.y);
+			int width = Math.abs(start.x - end.x);
+			int height = Math.abs(start.y - end.y);
+
+			// selectedBtn에 따라 다르게 그림
+			switch(selectedBtn) {
+				case "사각":
+					g.drawRect(x, y, width, height);
+					break;
+				case "타원":
+					g.drawOval(x, y, width, height);
+					break;
+				case "직선":
+					g.drawLine(start.x, start.y, end.x, end.y);
+					break;
+			}
+
+			// 이미 저장된 모양에 따라 화면에 그림을 그림
+			for (Shape shape : shapeArray) {
+				if (shape instanceof Rectangle) {
+					((Rectangle) shape).draw(g);
+				} else if (shape instanceof Circle) {
+					((Circle) shape).draw(g);
+				} else if (shape instanceof Line) {
+					((Line) shape).draw(g);
+				}
+			}
+		}
+		private class mouseListener extends MouseAdapter {
+			@Override
+			public void mousePressed(MouseEvent e) {
+				start = e.getPoint();
+			}
+			@Override
+			public void mouseDragged(MouseEvent e) {
+				end = e.getPoint();
+				repaint();
+			}
+			@Override
+			public void mouseReleased(MouseEvent e) {
+				end = e.getPoint();
+
+				Shape shp = null;
+				if (selectedBtn.equals("사각")) shp = new Rectangle(Math.min(start.x, end.x), Math.min(start.y, end.y), Math.abs(start.x - end.x), Math.abs(start.y - end.y));
+				else if (selectedBtn.equals("타원") )shp = new Circle(Math.min(start.x, end.x), Math.min(start.y, end.y), Math.abs(start.x - end.x), Math.abs(start.y - end.y));
+				else if (selectedBtn.equals("직선")) shp = new Line(start.x, start.y, end.x, end.y);
+				shapeArray.add(shp);
+				repaint();
+			}
 		}
 	}
 	
@@ -67,5 +133,49 @@ public class FigureEditor extends JFrame {
 	
 	public static void main(String[] args) {
 		new FigureEditor();
+	}
+}
+
+class Shape {
+	int x, y;
+	public Shape(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+}
+class Rectangle extends Shape {
+	int width, height;
+	public Rectangle(int x, int y, int width, int height) {
+		super(x, y);
+		this.width = width;
+		this.height = height;
+	}
+
+	public void draw(Graphics g) {
+		g.drawRect(x, y, width, height);
+	}
+}
+class Circle extends Shape {
+	int width, height;
+	public Circle(int x, int y, int width, int height) {
+		super(x, y);
+		this.width = width;
+		this.height = height;
+	}
+
+	public void draw(Graphics g) {
+		g.drawOval(x, y, width, height);
+	}
+}
+class Line extends Shape {
+	int x2, y2;
+	public Line(int x1, int y1, int x2, int y2) {
+		super(x1, y1);
+		this.x2 = x2;
+		this.y2 = y2;
+	}
+
+	public void draw(Graphics g) {
+		g.drawLine(x, y, x2, y2);
 	}
 }
